75a8791ea2fd8bc7d9063916c99662d2a10ce981
F. Cordero <fco.coma@gmail.com>
2021-10-26 22:06:36 -0500

    Abstract multitype Storetypes

--- src/cryzzly/dev/abstract_cell.cr
+++ src/cryzzly/dev/abstract_cell.cr
@@ -1,22 +1,12 @@
 require "spec"
+require "./multitype_defs"
+
 abstract class AbstractCell
   abstract def eval_value
 end
 
 class Cell < AbstractCell
-  STORE_MAPPINGS = [Int8, Int16, Int32, Int64, Float32, Float64, Time, Bool]
-
-  macro define_datatype
-    alias StoreTypes = Union({{*STORE_MAPPINGS}})
-  end
-
-  macro add_store_types(type)
-    {{STORE_MAPPINGS.push(type)}}
-  end
-
-  add_store_types(String)
-  define_datatype
-
+  include Multitype
   def eval_value
     @value
   end
